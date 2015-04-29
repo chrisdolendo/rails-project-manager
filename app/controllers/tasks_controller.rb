b0cd0192e054dc5e41c41cdb5bed5d14b2c3e792
Christine Dolendo <chris.dolendo@gmail.com>
2015-04-29 12:24:42 -0700

    Tasks can be created and deleted with project show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,29 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def create
+    @project = Project.find(params[:project_id])
+    @task = Task.create(task_params)
+    @project.tasks << @task
+    redirect_to project_path(@project)
+  end
+
+  def destroy
+    @task.destroy
+    respond_to do |format|
+      format.html { redirect_to root_url }
+      format.json { render :json => "success" }
+    end    
+  end 
+
+  private
+      # Use callbacks to share common setup or constraints between actions.
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :description, :difficulty_level)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 ProjectManager::Application.routes.draw do
   devise_for :users
-  resources :projects
   root to: 'pages#home'
 
+  resources :projects do
+    resources :tasks
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
